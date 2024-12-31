7e74ff1f825d07114c1ba02e61650b08870e760e
cyberforge1 <cyberforge1@gmail.com>
2024-12-31 12:28:57 +1100

    addition squares of sorted arrays

---
+++ arrays/squares_of_sorted_array.py
@@ -0,0 +1,33 @@
+# Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+# Example 1:
+
+# Input: nums = [-4,-1,0,3,10]
+# Output: [0,1,9,16,100]
+# Explanation: After squaring, the array becomes [16,1,0,9,100].
+# After sorting, it becomes [0,1,9,16,100].
+# Example 2:
+
+# Input: nums = [-7,-3,2,3,11]
+# Output: [4,9,9,49,121]
+ 
+# Constraints:
+
+# 1 <= nums.length <= 104
+# -104 <= nums[i] <= 104
+# nums is sorted in non-decreasing order.
+ 
+
+# Follow up: Squaring each element and sorting the new array is very trivial, could you find an O(n) solution using a different approach?
+
+class Solution(object):
+    def sortedSquares(self, nums):
+        new_arr = []
+        for num in nums:
+            print(num)
+            new_arr.append(num**2)
+        return sorted(new_arr)
+        
+        
+print(Solution().sortedSquares([-7,-3,2,3,11]))
+
+# python squares_of_sorted_array.py
