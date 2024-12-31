b57a97e50ce7045224a921ca784e5d81b38556b8
cyberforge1 <cyberforge1@gmail.com>
2024-12-31 15:06:20 +1100

    addition of duplicate zeros

--- arrays/duplicate_zeros.py
+++ arrays/duplicate_zeros.py
@@ -21,30 +21,26 @@
 # 1 <= arr.length <= 104
 # 0 <= arr[i] <= 9
 
+            
 class Solution(object):
     def duplicateZeros(self, arr):
         """
         :type arr: List[int]
         :rtype: None Do not return anything, modify arr in-place instead.
         """
-        
+        i = 0
         n = len(arr)
         
-        i = 0
-        while i < len(arr):
+        while i < n:
             if arr[i] == 0:
-                print('This item is 0', arr[i])
-                print('This is the index position', i)
-                print('This is the array segment', arr[i:])
-                shifted_segement = arr[i+1:] = arr[i:-1]
-                print('This is the shifted segment', shifted_segement)
-
-                
-                
-                
-                
-                
+                for j in range(n - 1, i, -1):  
+                    arr[j] = arr[j - 1]
+                if i + 1 < n:
+                    arr[i + 1] = 0
+                i += 1
             i += 1
+        
+        print('Final modified array:', arr)
         
         
         
