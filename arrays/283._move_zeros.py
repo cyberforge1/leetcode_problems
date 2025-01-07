c0c2ba7ed23b39263c2f982a59398ddf253fc6a3
cyberforge1 <cyberforge1@gmail.com>
2025-01-07 16:59:59 +1100

    addition of move zeros

---
+++ arrays/283._move_zeros.py
@@ -0,0 +1,50 @@
+# Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+# Note that you must do this in-place without making a copy of the array.
+
+ 
+
+# Example 1:
+
+# Input: nums = [0,1,0,3,12]
+# Output: [1,3,12,0,0]
+# Example 2:
+
+# Input: nums = [0]
+# Output: [0]
+ 
+
+# Constraints:
+
+# 1 <= nums.length <= 104
+# -231 <= nums[i] <= 231 - 1
+ 
+
+# Follow up: Could you minimize the total number of operations done?
+
+
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        
+        n = len(nums)
+        writePointer = 0
+
+        for readPointer in range(n):
+            if nums[readPointer] != 0:
+                nums[writePointer] = nums[readPointer]
+                writePointer += 1
+
+        for i in range(writePointer, n):
+            nums[i] = 0
+
+        return nums
+
+        
+        
+print(Solution().moveZeroes([0,1,0,3,12]))
+
+# python arrays/283._move_zeros.py
