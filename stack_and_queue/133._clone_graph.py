8784e4f8369a446d211a09cef2fac70225e92cdc
cyberforge1 <cyberforge1@gmail.com>
2025-01-28 18:34:47 +1100

    edits to clone graph

--- stack_and_queue/133._clone_graph.py
+++ stack_and_queue/133._clone_graph.py
@@ -47,13 +47,13 @@
 # Explanation: This an empty graph, it does not have any nodes.
 
 
-"""
+
 # Definition for a Node.
 class Node(object):
     def __init__(self, val = 0, neighbors = None):
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
-"""
+
 
 class Solution(object):
     def cloneGraph(self, node):
