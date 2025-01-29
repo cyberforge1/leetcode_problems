32f06d2aafeaf76905adeb29eb249261fabd0684
cyberforge1 <cyberforge1@gmail.com>
2025-01-29 13:02:34 +1100

    recursive reverse string solution

---
+++ recursion/344._reverse_string.py
@@ -0,0 +1,57 @@
+# 344. Reverse String
+# Solved
+# Easy
+# Topics
+# Companies
+# Hint
+# Write a function that reverses a string. The input string is given as an array of characters s.
+
+# You must do this by modifying the input array in-place with O(1) extra memory.
+
+ 
+
+# Example 1:
+
+# Input: s = ["h","e","l","l","o"]
+# Output: ["o","l","l","e","h"]
+# Example 2:
+
+# Input: s = ["H","a","n","n","a","h"]
+# Output: ["h","a","n","n","a","H"]
+ 
+
+# Constraints:
+
+# 1 <= s.length <= 105
+# s[i] is a printable ascii character.
+
+
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
+        """
+        
+        def helper(left, right):
+            if left >= right:
+                return   
+            s[left], s[right] = s[right], s[left]
+            
+            helper(left + 1, right - 1)
+        
+        helper(0, len(s) - 1)     
+        
+        
+        
+
+# Input: s = ["h","e","l","l","o"]
+# Output: ["o","l","l","e","h"]
+
+# Input: s = ["H","a","n","n","a","h"]
+# Output: ["h","a","n","n","a","H"]
+        
+print(Solution().reverseString(s = ["h","e","l","l","o"]))
+
+# python recursion/344._reverse_string.py
+        
