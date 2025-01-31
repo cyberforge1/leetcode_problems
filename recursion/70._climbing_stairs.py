a2e767c439d8c16388ebde6ced9f74203ce42001
cyberforge1 <cyberforge1@gmail.com>
2025-01-31 23:17:49 +1100

    addition of climbing stairs

--- recursion/70._climbing_stairs.py
+++ recursion/70._climbing_stairs.py
@@ -31,10 +31,16 @@
 # 1 <= n <= 45
 
 
-class Solution(object):
+class Solution:
+    def __init__(self):
+        self.memo = {}
+
     def climbStairs(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        
+        if n in self.memo:
+            return self.memo[n]
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        self.memo[n] = self.climbStairs(n - 1) + self.climbStairs(n - 2)
+        return self.memo[n]
