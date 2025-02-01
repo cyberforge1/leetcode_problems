488fd7ae29134fd46e217f063752f2dac12bd800
cyberforge1 <cyberforge1@gmail.com>
2025-02-01 16:08:11 +1100

    addition of maximum depth of binary tree

--- recursion/104._maximum_depth_of_binary_tree.py
+++ recursion/104._maximum_depth_of_binary_tree.py
@@ -36,4 +36,10 @@ class Solution(object):
         :type root: Optional[TreeNode]
         :rtype: int
         """
+        if not root:
+            return 0
         
+        left_depth = self.maxDepth(root.left)
+        right_depth = self.maxDepth(root.right)
+        
+        return 1 + max(left_depth, right_depth)
