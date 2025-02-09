d5dd942a6015193b2e93c8f4cf36fc051844d9e9
cyberforge1 <cyberforge1@gmail.com>
2025-02-10 09:16:20 +1100

    find first and last position of element in a sorted array

---
+++ binary_search/34._find_first_and_last_position_of_element_in_sorted_array.py
@@ -0,0 +1,59 @@
+# 34. Find First and Last Position of Element in Sorted Array
+# Medium
+# Topics
+# Companies
+# Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+
+# If target is not found in the array, return [-1, -1].
+
+# You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+# Example 1:
+
+# Input: nums = [5,7,7,8,8,10], target = 8
+# Output: [3,4]
+# Example 2:
+
+# Input: nums = [5,7,7,8,8,10], target = 6
+# Output: [-1,-1]
+# Example 3:
+
+# Input: nums = [], target = 0
+# Output: [-1,-1]
+ 
+
+# Constraints:
+
+# 0 <= nums.length <= 105
+# -109 <= nums[i] <= 109
+# nums is a non-decreasing array.
+# -109 <= target <= 109
+
+
+class Solution(object):
+    def searchRange(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        def binarySearch(findFirst):
+            left, right = 0, len(nums) - 1
+            index = -1
+            while left <= right:
+                mid = (left + right) // 2
+                if nums[mid] < target:
+                    left = mid + 1
+                elif nums[mid] > target:
+                    right = mid - 1
+                else:
+                    index = mid
+                    if findFirst:
+                        right = mid - 1  # Search left side for first occurrence
+                    else:
+                        left = mid + 1   # Search right side for last occurrence
+            return index
+        
+        return [binarySearch(True), binarySearch(False)]
